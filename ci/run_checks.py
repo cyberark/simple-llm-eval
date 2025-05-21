8938d9add413141195647922b11e021651466210
Roy Ben Yosef <roy.benyosef@cyberark.com>
2025-05-21 10:53:35 +0300

    moved to ruff

--- ci/run_checks.py
+++ ci/run_checks.py
@@ -56,19 +56,6 @@ def main():
         description='Running pre-commit hooks...'
     )
 
-    # Styling
-    # Use diff to verify correct format in the pipeline
-    # yapf --diff -vv --style=.config/.yapf_style -r --parallel simpleval
-    run_command(
-        cmd='yapf --in-place -vv --style=.config/.yapf_style -r --parallel simpleval',
-        description='Running yapf formatter...'
-    )
-
-    run_command(
-        cmd='flake8 --config .config/.flake8 simpleval',
-        description='Running flake8 linter...'
-    )
-
     # Complexity
     run_command(
         cmd='radon cc -n B simpleval',
@@ -80,19 +67,7 @@ def main():
         description='Running xenon complexity checks...'
     )
 
-    # Security
-    run_command(
-        cmd='bandit -r simpleval -lll',
-        description='Running bandit security checks...'
-    )
-
     update_requirements_txt()
-
-    # Linter
-    run_command(
-        cmd='pylint --recursive=y simpleval --rcfile .config/.pylintrc',
-        description='Running pylint linter...'
-    )
 
     if args.with_coverage:
         print(f'Running coverage checks with fail-under={COVERAGE_FAIL_UNDER}...')

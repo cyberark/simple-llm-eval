26be7b85479f27dd2c23d46735ebaa85af5d4178
Roy Ben Yosef <roy.benyosef@cyberark.com>
2025-05-21 19:36:46 +0300

    pre-pr fixes

--- ci/run_checks.py
+++ ci/run_checks.py
@@ -29,7 +29,7 @@ def update_requirements_txt():
     cmd='uv pip compile pyproject.toml --no-header -o {requirements_path}'
 
     run_command(
-        cmd=cmd.format(requirements_temp_path),
+        cmd=cmd.format(requirements_path=requirements_temp_path),
         description='Updating requirements-temp.txt...'
     )
 
@@ -40,7 +40,7 @@ def update_requirements_txt():
         print('Updating requirements.txt...')
         shutil.copyfile(requirements_temp_path, requirements_path)
         print('Command failed: requirements.txt updated, stopping')
-        sync_cmd = cmd.format(requirements_path)
+        sync_cmd = cmd.format(requirements_path=requirements_path)
         print(f'You must sync requirements.txt by calling: {sync_cmd}')
         sys.exit(1)
     else:

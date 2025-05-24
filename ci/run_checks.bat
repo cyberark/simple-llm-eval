6a98e5d76259c24339818ca63f852be4ee36ad4e
Roy Ben Yosef <60175164+roybeny@users.noreply.github.com>
2025-05-24 19:02:33 +0000

    Add batch files to run Python checks for main and React scripts (#74)

---
+++ ci/run_checks.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Batch file to run run_checks.py with Python on Windows
+python "%~dp0run_checks.py" %*
---
+++ ci/run_checks_react.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Batch file to run run_checks_react.py with Python on Windows
+python "%~dp0run_checks_react.py" %*
